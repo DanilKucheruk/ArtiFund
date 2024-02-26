c5a55f811ddf6d96fba0287347abb6344d73f535
Danil Kucheruk <danilkucheruk8@gmail.com>
2024-02-26 21:56:05 +0300

    init

---
+++ src/main/java/com/artifund/ArtifundApplication.java
@@ -0,0 +1,13 @@
+package com.artifund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifundApplication.class, args);
+	}
+
+}
